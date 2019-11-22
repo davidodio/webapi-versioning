61e8cd46703f7a648b1268e10d048513daa68413
David ODonnell <code@davidod.io>
2019-11-22 10:41:00 +0100

    feat: add versioning

---
+++ src/controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tapend.VersioningExample.V1
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get() => "Version 1";
+    }
+}
+
+namespace Tapend.VersioningExample.V2
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get() => "Version 2";
+    }
+}
